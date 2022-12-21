ce02f2f5c086b50411c4fbbdcc06d6b83620b424
derpflections <leehongyi91@gmail.com>
2022-12-21 19:32:41 +0800

    f

--- controller/app.js
+++ controller/app.js
@@ -6,7 +6,7 @@
 const express = require('express');
 const app = express()
 const bodyParser = require('body-parser')
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const storeDB = require("../model/actor.js");
 
 app.use(bodyParser.json());
@@ -14,29 +14,29 @@ app.use(urlencodedParser);
 
 
 //endpoint 1
-app.get("/actors/:actor_id", (req,res) =>{
+app.get("/actors/:actor_id", (req, res) => {
     var actor_id = req.params.actor_id
     storeDB.getActor(actor_id, (err, result) => {
-        if(err){
-            res.status(500).json({"error_msg":"Internal Server Error"}); //sends error message in json format
+        if (err) {
+            res.status(500).json({ "error_msg": "Internal Server Error" }); //sends error message in json format
         } else if (result === 204) {
             res.status(204).send(`No content. Record of given actor_id cannot be found.`)
             console.log(`No content. Record of given actor_id cannot be found.`) //prints error message to console
         } else {
             res.status(200).send(result); //sends result to postman/user.
         }
-    })    
+    })
 })
 
 //endpoint 2 
-app.get('/actors', (req, res) =>{
+app.get('/actors', (req, res) => {
     var limit = req.query.limit
     var offset = req.query.offset
-    storeDB.getActorOrder(limit, offset, (err, result) =>{
-        if(err){
-            res.status(500).json({"error_msg":"Internal Server Error"})//sends error message in json format
-        } else if (result === 400){
-            res.status(500).json({"error_msg":"Internal Server Error"})//sends error message in json format
+    storeDB.getActorOrder(limit, offset, (err, result) => {
+        if (err) {
+            res.status(500).json({ "error_msg": "Internal Server Error" })//sends error message in json format
+        } else if (result === 400) {
+            res.status(500).json({ "error_msg": "Internal Server Error" })//sends error message in json format
         } else {
             res.status(200).send(result) //sends result to postman/user
         }
@@ -44,20 +44,86 @@ app.get('/actors', (req, res) =>{
 })
 
 //endpoint 3
-app.post("/actors", (req, res) =>{
-    var actor_details = {'first_name' : req.body.first_name,'last_name': req.body.last_name}
-    storeDB.addActor(actor_details, (err, result) =>{
-        if (err){
-            res.status(500).json({"error_msg":"Internal Server Error"}) //sends error message in json format w/ err 500
-        } else if (result == 400){
-            res.status(400).json({"error_msg":"missing data"}) //sends error msg in json format w/ err 400
+app.post("/actors", (req, res) => {
+    var actor_details = { 'first_name': req.body.first_name, 'last_name': req.body.last_name }
+    storeDB.addActor(actor_details, (err, result) => {
+        if (err) {
+            res.status(500).json({ "error_msg": "Internal Server Error" }) //sends error message in json format w/ err 500
+        } else if (result == 400) {
+            res.status(400).json({ "error_msg": "missing data" }) //sends error msg in json format w/ err 400
         } else {
-            res.status(201).json({"actor_id": result.insertId.toString()}) //sends successful msg
+            res.status(201).json({ "actor_id": result.insertId.toString() }) //sends successful msg
         }
     })
-
 })
 
+//endpoint 4
+app.put("/actors/:actor_id", (req, res) => {
+    var actor_id = req.params.actor_id
+    var actor_details = req.body
+    console.log(actor_details)
+    storeDB.updateActor(actor_id, actor_details, (err, result) => {
+        if (err) {
+            res.status(500).json({ "error_msg": "Internal server error" })
+        } else if (result == 204) {
+            res.status(204).send(`No Content. Record of given actor_id cannot be found.`)
+        } else {
+            res.status(200).json({ "success_msg": "record updated" })
+        }
+    })
+})
 
+//endpoint 5
+app.delete("/actors/:actor_id", (req, res) => {
+    var actor_id = req.params.actor_id
+    storeDB.deleteActor(actor_id, (err, result) => {
+        if (err) {
+            res.status(500).json({ "error_msg": "Internal server error" })
+        } else if (result == 204) {
+            res.status(204).send(`No Content. Record of given actor_id cannot be found.`)
+        } else {
+            res.status(200).json({ "success_msg": "actor deleted" })
+        }
+    })
+})
+
+//endpoint 6 
+app.get("/film_categories/:category_id/films", (req, res) => {
+    var category_id = req.params.category_id
+    storeDB.getCategory(category_id, (err, result) => {
+        if (err) {
+            res.status(500).json({ "error_msg": "Internal server error" })
+        } else {
+            for (i = 0; i < result.length; i++) {
+                result[i].release_year = result[i].release_year.toString()
+                result[i].duration = result[i].duration.toString()
+            }
+            res.status(200).json(result)
+        }
+    })
+})
+
+//endpoint 7 
+app.get("/customer/:customer_id/payment", (req, res) => {
+    var customer_id = req.params.customer_id
+    var start_date = req.query.start_date
+    var end_date = req.query.end_date
+    storeDB.getPaymentDetails(customer_id, start_date, end_date, (err, result) => {
+        totalSum = 0
+        if (err) {
+            res.status(500).json({ "error_msg": "Internal server error" })
+        } else {
+            for (i = 0; i < result.length; i++){
+                totalSum += result[i].amount
+                result[i].amount = result[i].amount.toString()
+            }
+            totalSum = totalSum.toFixed(2)
+            if(result.length == 0){
+                totalSum = "0"
+            }
+            res.status(200).json({"rental": result, "total":totalSum})
+        }
+    })
+})
 
 module.exports = app
--- model/actor.js
+++ model/actor.js
@@ -4,31 +4,31 @@
 
 var db = require("./databaseConfig.js")
 
-function bodyChecker (var1, var2){
+function bodyChecker(var1, var2) {
     return (var1 == undefined || var1 == "" || var2 == undefined || var2 == "")
 }
 
 
 var storeDB = {
-    getActor:(actor_id, callback) => {
+    getActor: (actor_id, callback) => {
         var conn = db.getConnection();
         conn.connect((err) => {
-            if(err){
+            if (err) {
                 console.log(err);
                 return callback(err, null)
             } else {
                 console.log("Connected to database!")
                 var sql = `SELECT actor_id, first_name, last_name FROM actor WHERE actor_id = ?`
-                conn.query(sql, actor_id, (err,res) =>{
+                conn.query(sql, actor_id, (err, res) => {
                     conn.end();
-                    if(err){
+                    if (err) {
                         console.log(err);
                         return callback(err, null);
-                    } else if (res[0] === undefined){
-                        return callback (null, 204)
+                    } else if (res[0] === undefined) {
+                        return callback(null, 204)
                     } else {
                         res[0]['actor_id'] = res[0]['actor_id'].toString()
-                        return callback (null, res[0])
+                        return callback(null, res[0])
                     }
                 })
             }
@@ -38,21 +38,21 @@ var storeDB = {
     getActorOrder: (limit, offset, callback) => {
         var conn = db.getConnection()
         conn.connect((err) => {
-            if(err){
+            if (err) {
                 console.log(err)
                 return callback(err, null)
             } else {
                 console.log(`Connected to database!`)
                 var sql = `SELECT actor_id, first_name, last_name FROM actor ORDER BY first_name LIMIT ? OFFSET ?`;
-                conn.query (sql, [parseInt(limit), parseInt(offset)], (err, res) =>{
-                    if(err){
+                conn.query(sql, [parseInt(limit), parseInt(offset)], (err, res) => {
+                    if (err) {
                         console.log(err)
                         return callback(err, null)
-                    } else if (bodyChecker(limit, offset)) { 
-                        return callback (null, 400)
+                    } else if (bodyChecker(limit, offset)) {
+                        return callback(null, 400)
                     } else {
-                        for(i = 0 ; i < res.length ; i++){
-                            res[i].actor_id = res[i].actor_id.toString() 
+                        for (i = 0; i < res.length; i++) {
+                            res[i].actor_id = res[i].actor_id.toString()
                         }
                         return callback(null, res)
                     }
@@ -64,27 +64,130 @@ var storeDB = {
     addActor: (actor_details, callback) => {
         var conn = db.getConnection()
         conn.connect((err) => {
-            if(err){ 
+            if (err) {
                 console.log(err)
                 return callback(err, null)
             } else {
                 console.log(`Connected to database!`)
-                var sql = `INSERT INTO actor (first_name , last_name) VALUES (?, ?)`    
-                if (bodyChecker(actor_details.first_name, actor_details.last_name)){
-                    return callback (null, 400)
+                var sql = `INSERT INTO actor (first_name , last_name) VALUES (?, ?)`
+                if (bodyChecker(actor_details.first_name, actor_details.last_name)) {
+                    return callback(null, 400)
                 } else {
                     conn.query(sql, [actor_details.first_name.toUpperCase(), actor_details.last_name.toUpperCase()], (err, res) => {
                         conn.end()
-                        if(err){
+                        if (err) {
                             console.log(err)
-                            return callback (err, null)
+                            return callback(err, null)
                         } else {
-                            return callback (null, res)
+                            return callback(null, res)
                         }
                     })
                 }
             }
         })
+    },
+
+    updateActor: (actor_id, actor_details, callback) => {
+        var conn = db.getConnection()
+        conn.connect((err) => {
+            if (err) {
+                console.log(err)
+                return callback(err, null)
+            } else {
+                console.log(`Connected to database!`)
+                var sql = `UPDATE actor SET first_name = ? , last_name = ? WHERE actor_id = ?`
+                conn.query(sql, [actor_details.first_name, actor_details.last_name, actor_id], (err, res) => {
+                    conn.end()
+                    if (err) {
+                        console.log(err)
+                        return callback(err, null)
+                    } else if (res.affectedRows == 0){
+                        return callback(err, 204)
+                    } else {
+                        return callback(null, res)
+                    }
+                })
+            }
+        })
+    },
+
+    deleteActor: (actor_id, callback) => {
+        var conn = db.getConnection()
+        conn.connect((err) => {
+            if (err) {
+                console.log(err)
+                return callback(err, null)
+            } else {
+                console.log(`Connected to database!`)
+                var sql = `DELETE FROM film_actor WHERE actor_id = ?`
+                conn.query(sql, actor_id, (err, res) => {
+                    if (err) {
+                        console.log(err)
+                        return callback(err, null)
+                    } else {
+                        var sql = `DELETE FROM actor WHERE actor_id = ?`
+                        conn.query(sql, actor_id, (err, res) => {
+                            conn.end()
+                            if (err) {
+                                console.log(err)
+                                return callback(err, null)
+                            } else if (res.affectedRows == 0) {
+                                console.log(`Actor not found!`)
+                                return callback(null, 204)
+                            } else {
+                                console.log(res)
+                                return callback(null, res)
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    },
+
+    getCategory: (category_id, callback) => {
+        var conn = db.getConnection()
+        conn.connect((err) =>{
+            if(err){
+                console.log(err)
+                return callback(err, null)
+            } else {
+                console.log(`Connected to database!`)
+                var sql = `SELECT f.film_id, f.title, cat.name, f.rating, f.release_year, f.length as duration FROM film f, film_category fc, category cat WHERE f.film_id = fc.film_id AND fc.category_id = cat.category_id AND cat.category_id = ? ORDER BY film_id `
+                conn.query(sql, category_id, (err, res) =>{
+                    conn.end()
+                    if(err){
+                        console.log(err)
+                        return callback (err, null)
+                    } else {
+                        return callback(null, res)
+                    }
+                })
+            }
+        })
+    },
+
+    getPaymentDetails: (customer_id, start_date, end_date, callback) =>{
+        var conn = db.getConnection()
+        conn.connect((err) =>{
+            if(err){
+                console.log(err)
+                return callback(err, null)
+            } else {
+                console.log(`Connected to database!`)
+                var sql = `SELECT f.title, f.rental_rate AS amount, r.rental_date AS payment_date FROM customer c, inventory i, rental r, film f WHERE i.film_id = f.film_id AND r.customer_id = c.customer_id AND r.inventory_id = i.inventory_id AND c.customer_id = ? AND rental_date > ? AND rental_date < ?`
+                conn.query(sql, [customer_id, start_date, end_date], (err, res) =>{
+                    conn.end()
+                    if(err){
+                        console.log(err)
+                        return callback(err, null)
+                    } else {
+                        return callback (null, res)
+                    }
+                })
+            }
+        })
+
     }
 }
 
